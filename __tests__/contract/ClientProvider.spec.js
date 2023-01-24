daf82dcf586ef2a5234f6ca4955b9115694a9382
amandaremessa <amanda.antunes@remessaonline.com.br>
2023-01-24 18:01:07 -0300

    test(provider): our first contract test on provider side is working! :D

--- __tests__/contract/ClientProvider.spec.js
+++ __tests__/contract/ClientProvider.spec.js
@@ -1,10 +1,11 @@
-import { Verifier } from "@pact-foundation/pact";
+const path = require("path");
+const { Verifier } = require("@pact-foundation/pact");
 
 describe("Cliente Provider", ()=>{
     it("should verify the contract", async()=>{
         return new Verifier({
             providerBaseUrl: 'http://localhost:3333', // <- location of your running provider
-            pactUrls: [ path.resolve(process.cwd(), "../../pacts/frontend-clients-service.json") ],
+            pactUrls: [ path.resolve(process.cwd(), "./pacts/frontend-clients-service.json")],
           })
             .verifyProvider()
             .then(() => {
